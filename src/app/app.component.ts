aae4eaac36f4450f970e2d6cf5a98c85c818373e
filipemendes89 <filipemendes89@hotmail.com>
2023-09-16 16:04:47 -0300

    feat: collapse do menu lateral e icones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,14 @@ import { PoMenuItem } from '@po-ui/ng-components'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
 
   readonly menus: Array<PoMenuItem> = [
-    { label: 'Inicio', link: '/' },
-    { label: 'Pessoas', link: '/people' },
-    { label: 'Unidades', link: '/unit' }
+    { label: 'Inicio', link: '/', icon: 'po-icon-home', shortLabel: 'Início' },
+    { label: 'Pessoas', link: '/people', icon: 'po-icon-user', shortLabel: 'Pessoas' },
+    { label: 'Unidades', link: '/unit', icon: 'po-icon-company', shortLabel: 'Unidades' }
   ];
 
   private onClick() {
