c671421f1031fc60f7a239ea1b1a45c043258c2c
filipemendes89 <filipemendes89@hotmail.com>
2023-09-22 19:39:06 -0300

    feat: class stepper wip

--- src/app/class/class-new/class-new.component.ts
+++ src/app/class/class-new/class-new.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core'
-import { PoDynamicFormField, PoStepperOrientation } from '@po-ui/ng-components'
+import { PoDynamicFormField, PoDynamicViewField, PoStepperOrientation } from '@po-ui/ng-components'
 import { ClassService } from '../class.service'
 
 @Component({
@@ -10,6 +10,47 @@ import { ClassService } from '../class.service'
 })
 export class ClassNewComponent {
   public readonly orientation:PoStepperOrientation = PoStepperOrientation.Horizontal
+
+  public readonly fieldsSecondStep: Array<PoDynamicFormField> = [
+    {
+      property: 'teacher',
+      label: 'Professor',
+      gridColumns: 8,
+      gridSmColumns: 12,
+      optionsService: 'https://64f38ec0edfa0459f6c6aba4.mockapi.io/condomynium/api/v1/people?tipo=Professor',
+      fieldLabel: 'nome',
+      fieldValue: 'id',
+    },
+    {
+      property: 'sport',
+      divider: 'Esporte',
+      label: 'Esporte',
+      gridColumns: 8,
+      gridSmColumns: 12,
+      optional: false,
+      options: ['Beach Volley', 'Futevolei', 'Beach Tennis', 'Funcional'],
+      optionsMulti: false,
+    },
+    {
+      property: 'classSize',
+      label: 'Max. Alunos',
+      type: 'number',
+      gridColumns: 2,
+      maxValue: 10000,
+      errorMessage: 'Invalid number.'
+    },
+  ]
+
+  readonly detailFields: Array<PoDynamicViewField> = [
+    { property: 'sport', label: 'Esporte', divider: 'Dados da Aula', gridColumns: 6 },
+    { property: 'date', label: 'Data', type: 'date', gridColumns: 6 },
+    { property: 'time', label: 'Horário', gridColumns: 2 },
+    { property: 'court', label: 'Quadra', gridColumns: 4 },
+    { property: 'teacher', divider: 'Detalhes', label: 'Professor', gridColumns: 6 },
+    { property: 'people', label: 'Max. Alunos', gridColumns: 2 },
+  ];
+
+  detailValue:any = {} 
 
   isHideLoading = true
   availabeCourts:any = []
@@ -25,13 +66,15 @@ export class ClassNewComponent {
       
     }]
 
-    yes() {
-      return true
+    onLoad() {
+      console.log(this.detailValue)
+      return this.detailValue
     }
 
     constructor(private classService: ClassService) {}
 
     getAvailabeCourts(evento: any) {
+      this.detailValue.date = evento
       this.isHideLoading = false
       this.classService.getAvailabeCourts(this.serviceApi, evento).subscribe(
         (data:any) => this.availabeCourts = data,
@@ -40,6 +83,7 @@ export class ClassNewComponent {
     }
 
     onSelectHour(time:any, item:any) {
-      console.log(time,item)
+      this.detailValue.court = item.courtName
+      this.detailValue.time = time
     }
 }
