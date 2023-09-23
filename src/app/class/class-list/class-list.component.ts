3842a314f15f5523463788895d208ce4d8c90111
filipemendes89 <filipemendes89@hotmail.com>
2023-09-23 18:12:51 -0300

    feat: class list com alunos

--- src/app/class/class-list/class-list.component.ts
+++ src/app/class/class-list/class-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ClassService } from '../class.service';
-import { PoBreadcrumb, PoNotificationService, PoPageAction } from '@po-ui/ng-components';
+import { PoBreadcrumb, PoListViewAction, PoListViewLiterals, PoNotificationService, PoPageAction } from '@po-ui/ng-components';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-class-list',
@@ -15,6 +16,25 @@ export class ClassListComponent {
     url: 'class/new'
   }]
 
+  readonly actionsView: Array<PoListViewAction> = [
+    {
+      label: 'Aluno',
+      action: (e:any) => console.log(e),
+      icon: 'po-icon-plus'
+    },
+    {
+      label: 'Editar',
+      action: (e:any) => this._router.navigateByUrl(`/court/class/${e.id}`),
+      icon: 'po-icon-edit'
+    }
+  ];
+
+  public readonly literals:PoListViewLiterals = {
+    showDetails: 'Lista de alunos',
+    hideDetails: 'Fechar',
+    noData: 'Sem aulas'
+  }
+
   public readonly breadcrumb:PoBreadcrumb = {
     items: [{ label: 'Home', link: '/' }, { label: 'Class' }]
   }
@@ -22,19 +42,23 @@ export class ClassListComponent {
   classApi = 'https://64f38ec0edfa0459f6c6aba4.mockapi.io/condomynium/api/v1/class'
   isHideLoading = true
 
-  constructor(private classService: ClassService, private poNotification: PoNotificationService) {}
+  constructor(private classService: ClassService, private poNotification: PoNotificationService, private _router: Router) {}
   classes:any
 
   ngOnInit() {
     this.isHideLoading = false
     this.classService.getClass(this.classApi).subscribe(
     (data: any) => { 
-      this.classes = data 
+      this.classes = data.items 
       this.isHideLoading = true
     }, 
     (error) => {
       this.poNotification.error(error)
       this.isHideLoading = true
     })
+  }
+
+  showDetail(e?:any) {
+    console.log('a')
   }
 }
--- src/app/class/class.module.ts
+++ src/app/class/class.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 
-import { PoAvatarModule, PoButtonModule, PoContainerModule, PoDynamicModule, PoFieldModule, PoLoadingModule, PoPageModule, PoStepperModule, PoWidgetModule } from '@po-ui/ng-components'
+import { PoAvatarModule, PoButtonModule, PoContainerModule, PoDynamicModule, PoFieldModule, PoInfoModule, PoListViewModule, PoLoadingModule, PoPageModule, PoStepperModule, PoWidgetModule } from '@po-ui/ng-components'
 import { ClassNewComponent } from './class-new/class-new.component'
 import { ClassRoutingModule } from './class-routing.module'
 import { PoPageDynamicEditModule } from '@po-ui/ng-templates';
 import { ClassListComponent } from './class-list/class-list.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 
 @NgModule({
@@ -26,7 +27,9 @@ import { ClassListComponent } from './class-list/class-list.component'
     PoLoadingModule,
     PoPageDynamicEditModule,
     PoDynamicModule,
-    PoAvatarModule
+    PoAvatarModule,
+    PoListViewModule,
+    PoInfoModule
   ]
 })
 export class ClassModule { }
