513b0bdfd1723f325c60cc981d7a1409b3e5e058
Filipe Mendes <filipemendes89@hotmail.com>
2023-09-25 18:47:50 -0300

    chore: start types

---
+++ src/app/court/@types.ts
@@ -0,0 +1,10 @@
+export interface ICourt {
+    createdAt: string
+    name: string
+    avatar: string
+    type: string
+    sports: string[]
+    active: boolean
+    id: string
+    calendar: string
+}
