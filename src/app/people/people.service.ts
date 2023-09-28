a491e3492370da7a16a4f0061941eb9d44742fa5
filipe.mendes <filipe.mendes@totvs.com.br>
2023-09-28 12:53:26 -0300

    fix: people service + yml

--- src/app/people/people.service.ts
+++ src/app/people/people.service.ts
@@ -1,29 +1,19 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 
-const code = 'u3FbvFbmYspLyHXrquopiRwOpSb5dtzDoLJkkuxvaeT8AzFuNmrNxw=='
 @Injectable()
 export class PeopleService {
   constructor(public http: HttpClient) {}
 
   postPerson(endpoint: any, pessoa: any) {
-    return this.http.post(endpoint, pessoa, { params: {
-        code
-      }
-    })
+    return this.http.post(endpoint, pessoa)
   }
 
   putPerson(endpoint: any, pessoa: any) {
-    return this.http.put(endpoint, pessoa, { params: {
-        code
-      }
-    })
+    return this.http.put(endpoint, pessoa)
   }
 
   getPeople(endpoint:any) {
-    return this.http.get(endpoint, { params: {
-        code
-      }
-    })
+    return this.http.get(endpoint)
   }
 }
