b8677de96be05907befbd8ef84529635294d6d6e
filipe.mendes <filipe.mendes@totvs.com.br>
2023-09-29 14:10:20 -0300

    feat: toolbar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
-import { PoBreadcrumb, PoMenuItem, PoPageAction, PoTableColumn } from '@po-ui/ng-components'
+import { AuthService } from '@auth0/auth0-angular'
+import { PoBreadcrumb, PoMenuItem, PoTableColumn, PoToolbarAction } from '@po-ui/ng-components'
 
 @Component({
   selector: 'app-home',
@@ -7,21 +8,26 @@ import { PoBreadcrumb, PoMenuItem, PoPageAction, PoTableColumn } from '@po-ui/ng
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  
+  constructor(private auth: AuthService) {}
   paymentLink: string = 'https://www.google.com.br/search?q=days+to+payment';
   itemsDetails: Array<any> = []
   titleDetailsModal: string = ''
   typeChart: string = 'line';
-
-  readonly actions:PoPageAction[] = [
-    { label: 'Logout', url: '/logout', icon: 'po-icon-exit' }
-  ]
+  
+  readonly profileActions:PoToolbarAction[] = [
+    { label: 'Perfil', url: '/profile', icon: 'po-icon-user', type: 'primary' },
+    { label: 'Logout', url: '/logout', icon: 'po-icon-exit', type: 'danger' }
+  ];
+  
   readonly menus: Array<PoMenuItem> = [
     { label: 'Inicio', link: '/', icon: 'po-icon-home', shortLabel: 'Início' },
     { label: 'Pessoas', link: '/people', icon: 'po-icon-user', shortLabel: 'Pessoas' },
     { label: 'Aulas', link: '/class', icon: 'po-icon-calendar-ok', shortLabel: 'Aulas' },
     { label: 'Quadras', link: '/court', icon: 'po-icon-target', shortLabel: 'Quadras' },
     { label: 'Calendários', link: '/calendar', icon: 'po-icon-calendar', shortLabel: 'Calendários' },
-    { label: 'Estoque', link: '/object', icon: 'po-icon-stock', shortLabel: 'Estoque' }
+    { label: 'Estoque', link: '/object', icon: 'po-icon-stock', shortLabel: 'Estoque' },
+    { label: 'Sair', link: '/logout', icon: 'po-icon-exit', shortLabel: 'Sair',  }
   ];
 
   public readonly breadcrumb: PoBreadcrumb = {
@@ -48,7 +54,22 @@ export class HomeComponent {
     { dateUpdate: '12-05-2017', statement: '2000' }
   ];
 
+  profile:any
+
   daysPayment() {
     window.open(this.paymentLink, '_blank');
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+      this.auth.user$.subscribe((data:any) => { 
+        this.profile = {
+          avatar: data.picture,
+          title: data.name,
+          subTitle: data.email
+        }
+        console.log(data) 
+      } )
   }
 }
